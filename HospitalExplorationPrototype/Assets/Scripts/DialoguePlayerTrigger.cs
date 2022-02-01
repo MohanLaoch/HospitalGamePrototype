5ee9d80a1cff61f006e501dcd48735053d1b4ca1
James Mohan <c20305011@mytudublin.ie>
2022-02-01 18:43:14 +0000

    Minor text, code and dialogue changes

--- HospitalExplorationPrototype/Assets/Scripts/DialoguePlayerTrigger.cs
+++ HospitalExplorationPrototype/Assets/Scripts/DialoguePlayerTrigger.cs
@@ -11,6 +11,12 @@ public class DialoguePlayerTrigger : MonoBehaviour
     public GameObject Receptionist4;
     public GameObject Receptionist5;
 
+    public GameObject StatsNurseIndicator;
+    public GameObject BloodsNurseIndicator;
+    public GameObject ChefIndicator;
+    public GameObject XRayNurseIndicator;
+    public GameObject DoctorIndicator;
+    
     public int spoken = 0;
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -20,6 +26,7 @@ public class DialoguePlayerTrigger : MonoBehaviour
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
             spoken = 1;
+            StatsNurseIndicator.SetActive(true);
         }
 
         if (other.gameObject.CompareTag("StatsNurse") && spoken == 1)
@@ -28,12 +35,15 @@ public class DialoguePlayerTrigger : MonoBehaviour
             spoken = 2;
             Receptionist1.SetActive(false);
             Receptionist2.SetActive(true);
+            StatsNurseIndicator.SetActive(false);
         }
         
         if (other.gameObject.CompareTag("Receptionist") && spoken == 2)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
             spoken = 3;
+            BloodsNurseIndicator.SetActive(true);
+
         }
 
         if(other.gameObject.CompareTag("BloodsNurse") && spoken == 3)
@@ -42,12 +52,15 @@ public class DialoguePlayerTrigger : MonoBehaviour
             spoken = 4;
             Receptionist2.SetActive(false);
             Receptionist3.SetActive(true);
+            BloodsNurseIndicator.SetActive(false);
+            
         }
         
         if (other.gameObject.CompareTag("Receptionist") && spoken == 4)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
             spoken = 5;
+            ChefIndicator.SetActive(true);
         }
         
         if (other.gameObject.CompareTag("Chef") && spoken == 5)
@@ -56,12 +69,14 @@ public class DialoguePlayerTrigger : MonoBehaviour
             spoken = 6;
             Receptionist3.SetActive(false);
             Receptionist4.SetActive(true);
+            ChefIndicator.SetActive(false);
         }
         
         if (other.gameObject.CompareTag("Receptionist") && spoken == 6)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
             spoken = 7;
+            XRayNurseIndicator.SetActive(true);
         }
         
         if (other.gameObject.CompareTag("X-RayNurse") && spoken == 7)
@@ -70,17 +85,20 @@ public class DialoguePlayerTrigger : MonoBehaviour
             spoken = 8;
             Receptionist4.SetActive(false);
             Receptionist5.SetActive(true);
+            XRayNurseIndicator.SetActive(false);
         }
         
         if (other.gameObject.CompareTag("Receptionist") && spoken == 8)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
             spoken = 9;
+            DoctorIndicator.SetActive(true);
         }
 
         if (other.gameObject.CompareTag("Doctor") && spoken == 9)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            DoctorIndicator.SetActive(false);
         }
 
   
