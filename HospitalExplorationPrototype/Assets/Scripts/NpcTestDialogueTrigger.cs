763c4dc9ef45093ce34e8e8c7c778c2f3bc77028
declanqc <72388492+declanqc@users.noreply.github.com>
2022-02-01 14:19:17 +0000

    fixed dialoguebox animation error

--- HospitalExplorationPrototype/Assets/Scripts/NpcTestDialogueTrigger.cs
+++ HospitalExplorationPrototype/Assets/Scripts/NpcTestDialogueTrigger.cs
@@ -6,6 +6,7 @@ public class NpcTestDialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
     public bool NearNPC = false;
+    public GameObject dialogueBox;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         NearNPC = true;
@@ -15,12 +16,16 @@ public class NpcTestDialogueTrigger : MonoBehaviour
     {
         NearNPC = false;
     }
-
+    private void Start()
+    {
+        dialogueBox.SetActive(false);
+    }
     private void Update()
     {
         if (NearNPC == true && Input.GetKeyDown("space"))
         {
             FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            gameObject.SetActive(true);
         }
     }
 }
