e8e5b875d3c2a3aef94d1db2b3256800a79e92d5
James Mohan <c20305011@mytudublin.ie>
2022-02-01 16:38:26 +0000

    God Gamer Code

--- HospitalExplorationPrototype/Assets/Scripts/DialoguePlayerTrigger.cs
+++ HospitalExplorationPrototype/Assets/Scripts/DialoguePlayerTrigger.cs
@@ -4,30 +4,81 @@ using UnityEngine;
 
 public class DialoguePlayerTrigger : MonoBehaviour
 {
+    
+    public GameObject Receptionist1;
+    public GameObject Receptionist2;
+    public GameObject Receptionist3;
+    public GameObject Receptionist4;
+    public GameObject Receptionist5;
+
+    public int spoken = 0;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Receptionist"))
+
+        if (other.gameObject.CompareTag("Receptionist") && spoken ==0)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
-           
+            spoken = 1;
         }
 
-        if(other.gameObject.CompareTag("StatsNurse"))
+        if (other.gameObject.CompareTag("StatsNurse") && spoken == 1)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            spoken = 2;
+            Receptionist1.SetActive(false);
+            Receptionist2.SetActive(true);
         }
-
-        if(other.gameObject.CompareTag("BloodsNurse"))
+        
+        if (other.gameObject.CompareTag("Receptionist") && spoken == 2)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            spoken = 3;
         }
 
-        if (other.gameObject.CompareTag("Doctor"))
+        if(other.gameObject.CompareTag("BloodsNurse") && spoken == 3)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            spoken = 4;
+            Receptionist2.SetActive(false);
+            Receptionist3.SetActive(true);
+        }
+        
+        if (other.gameObject.CompareTag("Receptionist") && spoken == 4)
+        {
+            other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            spoken = 5;
+        }
+        
+        if (other.gameObject.CompareTag("Chef") && spoken == 5)
+        {
+            other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            spoken = 6;
+            Receptionist3.SetActive(false);
+            Receptionist4.SetActive(true);
+        }
+        
+        if (other.gameObject.CompareTag("Receptionist") && spoken == 6)
+        {
+            other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            spoken = 7;
+        }
+        
+        if (other.gameObject.CompareTag("X-RayNurse") && spoken == 7)
+        {
+            other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            spoken = 8;
+            Receptionist4.SetActive(false);
+            Receptionist5.SetActive(true);
+        }
+        
+        if (other.gameObject.CompareTag("Receptionist") && spoken == 8)
+        {
+            other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
+            spoken = 9;
         }
 
-        if (other.gameObject.CompareTag("Chef"))
+        if (other.gameObject.CompareTag("Doctor") && spoken == 9)
         {
             other.GetComponent<NpcTestDialogueTrigger>().DisplayText();
         }
